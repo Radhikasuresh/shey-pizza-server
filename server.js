6e0e916d4e60e3b6f472f2e09ed660215e9786d5
Radhikasuresh <radhikasuresh550@gmail.com>
2023-09-08 19:26:21 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+// const cors = require("cors");
+const Pizza = require("./models/pizzaModel");
+const app = express();
+const db = require("./db.js");
+const userRoute = require("./routes/userRoute");
+const pizzaRoute = require("./routes/pizzaRoute");
+const ordersRoute = require("./routes/ordersRoute");
+
+app.use(express.json());
+// app.use(
+//     cors({
+//       origin: "*",
+//     })
+//   );
+//
+
+app.use("/api/pizzas/", pizzaRoute);
+app.use("/api/users/", userRoute);
+app.use("/api/orders/", ordersRoute);
+
+app.get("/", (req, res) => {
+  res.send("server working🔥");
+});
+app.get("/getpizzas", (req, res) => {
+  Pizza.find({}, (err, docs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(docs);
+    }
+  });
+});
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server running on port🔥 ${port}`));
